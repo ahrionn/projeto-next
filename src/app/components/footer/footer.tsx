d087ec416e91b950ee1b08883f48d68b99114a8c
Luam Ahrion <luam_azf@hotmail.com>
2025-03-17 17:31:07 -0300

    minor changes

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -3,7 +3,7 @@ import styles from './footer.module.css'
 export default function Footer() {
     return (
         <footer className={styles.footer}>
-            <p>&copy; 2025 MinhaMarca. Todos os direitos reservados.</p>
+            <p>&copy; 2025 CRMOne. Todos os direitos reservados.</p>
         </footer>
     )
 }
